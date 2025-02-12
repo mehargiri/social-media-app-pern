4777d2cb405320336e1f9952911e6833f5ba4574
Mehar Giri <84210815+mehargiri@users.noreply.github.com>
2025-02-12 17:23:14 -0500

    Updated comment table's zod schemas to include more data validations

--- server/src/features/comment/comment.zod.schemas.ts
+++ server/src/features/comment/comment.zod.schemas.ts
@@ -1,5 +1,8 @@
 import { comment } from '@/db/schema/index.js';
 import { createInsertSchema } from 'drizzle-zod';
+import short, { SUUID } from 'short-uuid';
+
+const translator = short();
 
 export const createCommentSchema = createInsertSchema(comment, {
 	content: (schema) =>
@@ -7,14 +10,33 @@ export const createCommentSchema = createInsertSchema(comment, {
 			.min(1, 'Content is required')
 			.max(10000, 'Content cannot be more than 10,000 characters')
 			.trim(),
+	postId: (schema) =>
+		schema
+			.min(1, 'Post id is required')
+			.refine(
+				(value) => translator.validate(value),
+				'Valid post id is required'
+			),
+	commentLevel: (schema) =>
+		schema
+			.min(0, 'Comment level cannot be less than 0')
+			.max(2, 'Comment level cannot be more than 2'),
+	parentCommentId: (schema) =>
+		schema.refine(
+			(value) => translator.validate(value),
+			'Valid parent comment id is required'
+		),
 }).omit({
 	id: true,
 	userId: true,
 	createdAt: true,
 	updatedAt: true,
-	postId: true,
-	parentCommentId: true,
-	commentLevel: true,
 });
 
-export type CommentType = typeof createCommentSchema._type;
+export type CommentType = Omit<
+	typeof createCommentSchema._type,
+	'postId' | 'parentCommentId'
+> & {
+	postId: SUUID;
+	parentCommentId: SUUID | null;
+};
